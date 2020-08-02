db42aa81035d8e95b51a795538c2b2d7a9f42a63
Debra <48626342+DobroTora@users.noreply.github.com>
2020-08-02 20:54:00 +0100

    app skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import animalList from "./data.js";
+import AnimalTable from "./tables/AnimalTable";
+import AddAnimalForm from "./forms/AddAnimalForm";
+import EditAnimalForm from "./forms/EditAnimalForm";
 
-function App() {
+const App = () => {
+  const [animals, setAnimals] = useState(animalList);
+
+  const addAnimal = (animal) => {
+    animal.id = animals.length + 1;
+    setAnimals([...animals, animal]);
+  };
+
+  const deleteAnimal = (id) => {
+    setAnimals(animals.filter((animal) => animal.id !== id));
+  };
+
+  const [editing, setEditing] = useState(false);
+  const initialAnimal = { id: null, name: "", type: "", eatingHabits: "", extinct: "" };
+  const [currentAnimal, setCurrentAnimal] = useState(initialAnimal);
+
+  const editAnimal = (id, animal) => {
+    setEditing(true);
+    setCurrentAnimal(animal);
+  };
+
+  const updateAnimal = (newAnimal) => {
+    setAnimals(
+      animals.map((animal) => (animal.id === currentAnimal.id ? newAnimal : animal))
+    );
+    setCurrentAnimal(initialAnimal);
+    setEditing(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav>
+        <div className="nav-wrapper">
+              <h4>ANIMAL KINGDOM</h4>
+        </div>
+      </nav>
+      
+      <div className="row">
+        <div className="five columns">
+          {editing ? (
+            <div>
+              <h2>Edit animal</h2>
+              <EditAnimalForm
+                currentAnimal={currentAnimal}
+                setEditing={setEditing}
+                updateAnimal={updateAnimal}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add Animal</h2>
+              <AddAnimalForm addAnimal={addAnimal} />
+            </div>
+          )}
+        </div>
+        <div className="seven columns">
+          <h2>View Animals</h2>
+          <AnimalTable
+            animals={animals}
+            deleteAnimal={deleteAnimal}
+            editAnimal={editAnimal}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
