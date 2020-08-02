e51ee4562fdc96684325413be3446b4f1ffb443d
Debra <48626342+DobroTora@users.noreply.github.com>
2020-08-02 21:01:00 +0100

    Add data

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const animalList = [
+    {
+        id: 1,
+        name: 'Tiger',
+        type: 'Mammal',
+        eatingHabits: 'Carnivore',
+        extinction: 'no'
+    },
+    {
+        id: 2,
+        name: 'Deer',
+        type: 'Mammal',
+        eatingHabits: 'Herbivore',
+        extinction: 'no'
+    },
+];
+
+export default animalList;
